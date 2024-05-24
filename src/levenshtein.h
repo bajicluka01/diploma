3ca1222b2596c998d4874c9eb1c2f934532a2ece
Luka Bajić <bajicluka01@gmail.com>
2024-05-24 07:06:06 +0200

    Added sequential Levenshtein implementation (dynamic programming)

--- src/levenshtein.h
+++ src/levenshtein.h
@@ -5,9 +5,45 @@
 using namespace std;
 using namespace std::chrono;
 
+//dynamic programming solution
 int sequentialLevenshtein (string str1, string str2, int row, int column) {
-    //TODO
-    return 0;
+    //allocate
+    int** arr = new int*[row];
+    for(int i = 0; i < row; i++)
+        arr[i] = new int[column];
+
+    //initialize zeros
+    for (int i = 0; i < row; i++){
+        for (int j = 0; j < column; j++) {
+            arr[i][j] = 0;
+        }
+    }
+
+    for(int i = 0; i < row; i++) {
+        for (int j = 0; j < column; j++) {
+            //first row and column (basically empty string)
+            if (i == 0)
+                arr[i][j] = j;
+            if (j == 0)
+                arr[i][j] = i;
+            
+            //the rest of the table
+            if(i != 0 && j != 0) {
+                if (str1[i-1] == str2[j-1])
+                    arr[i][j] = arr[i-1][j-1];
+                else
+                    arr[i][j] = 1 + min(arr[i][j-1], min(arr[i-1][j], arr[i-1][j-1]));
+            }
+        }
+    }
+
+    int ret = arr[row-1][column-1];
+
+    //deallocate
+    for(int i = 0; i < row; i++)
+        delete[] arr[i];
+
+    return ret;
 }
 
 int parallelLevenshtein (string str1, string str2, int row, int column) {
--- src/main.cpp
+++ src/main.cpp
@@ -24,19 +24,41 @@ int main(int argc, char* argv[]){
     int row = str1.length()+1;
     int column = str2.length()+1;
 
+    //Longest Common Subsequence
     auto start = high_resolution_clock::now();
-    cout << "LCS: " << sequentialLCS(str1, str2, row, column) << "\n";
+    cout << "Sequential LCS solution: " << sequentialLCS(str1, str2, row, column) << "\n";
     auto finish = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(finish - start);
 
-    cout << "Sequential: " << duration.count() << "\n";
+    cout << "Sequential LCS duration: " << duration.count() << "\n";
 
     start = high_resolution_clock::now();
-    cout << "LCS: " << parallelLCS(str1, str2, row, column) << "\n";
+    cout << "Parallel LCS solution: " << parallelLCS(str1, str2, row, column) << "\n";
     finish = high_resolution_clock::now();
     duration = duration_cast<microseconds>(finish - start);
 
-    cout << "Parallel: " << duration.count() << "\n";
+    cout << "Parallel LCS duration: " << duration.count() << "\n";
+
+
+    str1 = "delete";
+    str2 = "replace";
+    row = str1.length()+1;
+    column = str2.length()+1;
+
+    //Levenshtein distance
+    start = high_resolution_clock::now();
+    cout << "Sequential Levenshtein solution: " << sequentialLevenshtein(str1, str2, row, column) << "\n";
+    finish = high_resolution_clock::now();
+    duration = duration_cast<microseconds>(finish - start);
+
+    cout << "Sequential Levenshtein duration: " << duration.count() << "\n";
+
+    start = high_resolution_clock::now();
+    cout << "Parallel Levenshtein solution: " << parallelLevenshtein(str1, str2, row, column) << "\n";
+    finish = high_resolution_clock::now();
+    duration = duration_cast<microseconds>(finish - start);
+
+    cout << "Parallel Levenshtein duration: " << duration.count() << "\n";
     
     return 0;
 }
