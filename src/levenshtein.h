3fec02d8fce3177ece5aa50d92a7d58622a4b1dd
Luka Bajić <bajicluka01@gmail.com>
2024-06-30 12:31:40 +0200

    Fixed bottomUp for Levenshtein and added merge (with one bug still to be fixed)

--- src/levenshtein.h
+++ src/levenshtein.h
@@ -21,6 +21,7 @@ int sequentialLevenshtein (string str1, string str2, int row, int column) {
         }
     }
 
+    /*
     //test if first characters match for the purposes of calculating first row and column more efficiently
     bool firstChar = str1[0] == str2[0];
 
@@ -36,12 +37,15 @@ int sequentialLevenshtein (string str1, string str2, int row, int column) {
                     arr[i][j] = firstChar ? 0 : i+1;
             }
         }
-    }
+    }*/
 
-    //rest of table
-    for(int i = 1; i < row; i++) {
-        for (int j = 1; j < column; j++) {          
-            if (str1[i] == str2[j])
+    for(int i = 0; i < row; i++) {
+        for (int j = 0; j < column; j++) {    
+            if (i == 0)
+                arr[i][j] = j;
+            else if (j == 0)
+                arr[i][j] = i;
+            else if (str1[i-1] == str2[j-1])
                 arr[i][j] = arr[i-1][j-1];
             else 
                 arr[i][j] = 1 + min(arr[i][j-1], min(arr[i-1][j], arr[i-1][j-1]));
@@ -115,6 +119,7 @@ int parallelLevenshtein (string str1, string str2, int row, int column) {
 
 //one thread calculates the top half of the table
 void topHalf(args& a) {
+    /*
     //test if first characters match for the purposes of calculating first row and column more efficiently
     bool firstChar = a.s1[0] == a.s2[0];
 
@@ -130,22 +135,27 @@ void topHalf(args& a) {
                     arr[i][j] = firstChar ? arr[i-1][j]+1 : i+1;
             }
         }
-    }
+    }*/
 
-    //rest of table
-    for(int i = 1; i < a.row; i++) 
-        for (int j = 1; j < a.col; j++)       
-            if (a.s1[i] == a.s2[j])
+    for(int i = 0; i <= a.row; i++) 
+        for (int j = 0; j < a.col; j++) {      
+            if (i == 0)
+                arr[i][j] = j;
+            else if (j == 0)
+                arr[i][j] = i;
+            else if (a.s1[i-1] == a.s2[j-1])
                 arr[i][j] = arr[i-1][j-1];
             else 
                 arr[i][j] = 1 + min(arr[i][j-1], min(arr[i-1][j], arr[i-1][j-1]));
+        }
 }
 
 //one thread calculates the bottom half of the table
 //TODO fix odd/even h
 void bottomHalf(args& a) {
-    int nrows = a.s1.length()-1;
+    int nrows = a.s1.length()+1;
 
+    /*
     bool lastChar = a.s1[a.s1.length()-1] == a.s2[a.s2.length()-1];
 
     //first row and column (can probably be further optimized)
@@ -160,43 +170,89 @@ void bottomHalf(args& a) {
                     arr[i][j] = lastChar ? arr[i+1][j]+1 : nrows-i+1;
             }
         }
-    }
+    }*/
 
-    //rest of table
-    for(int i = nrows-1; i >= a.row; i--) 
-        for(int j = a.col-2; j >= 0; j--) 
-            if(a.s1[i] == a.s2[j])
-                arr[i][j] = arr[i+1][j+1];
-            else
-                arr[i][j] = 1 + min(arr[i][j+1], min(arr[i+1][j], arr[i+1][j+1]));
+    for(int i = nrows-1; i >= a.row; i--) {
+        for (int j = a.col-1; j >= 0; j--) {    
+            if (i == nrows-1)
+                arr[i+1][j+1] = a.col-j-1;
+            else if (j == a.col-1)
+                arr[i+1][j+1] = nrows-i-1;
+            else if (a.s1[i] == a.s2[j])
+                arr[i+1][j+1] = arr[i+2][j+2];
+            else 
+                arr[i+1][j+1] = 1 + min(arr[i+1][j+2], min(arr[i+2][j+1], arr[i+2][j+2]));
+        }
+    }
 }
 
 //merges last rows of topHalf and bottomHalf
 int merge(int h, int row, int column) {
     int* temp = new int[column-1];
-    int currentMin = 0;
+    int currentMin = INT_MAX;
 
     for(int i = 1; i<column; i++) {
         temp[i] = arr[h][i] + arr[h+1][i];
 
-        if(temp[i] > currentMin)
+        if(temp[i] < currentMin)
             currentMin = temp[i];
 
     }
 
+    //sometimes the correct solution seems to be currentMin-1, TODO: verify why
     return currentMin;
+}
+
+int bottomUp (string str1, string str2, int row, int column) {
+
+    //allocate
+    int** arr = new int*[row];
+    for(int i = 0; i < row+1; i++)
+        arr[i] = new int[column];
+
+    //initialize zeros
+    for (int i = 0; i < row+1; i++){
+        for (int j = 0; j < column+1; j++) {
+            arr[i][j] = 0;
+        }
+    }
+
+    for(int i = row-1; i >= 0; i--) {
+        for (int j = column-1; j >= 0; j--) {    
+            if (i == row-1)
+                arr[i][j] = column-j-1;
+            else if (j == column-1)
+                arr[i][j] = row-i-1;
+            else if (str1[i] == str2[j])
+                arr[i][j] = arr[i+1][j+1];
+            else 
+                arr[i][j] = 1 + min(arr[i][j+1], min(arr[i+1][j], arr[i+1][j+1]));
+        }
+    }
+
+    cout << "\n";
+    for (int i = 0; i < row; i++){
+        for (int j = 0; j < column; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+    int ret = arr[row-1][column-1];
+
+    return ret;
 }
 
 //forward-backward approach with 2 threads
 int fb_levenshtein (string str1, string str2, int row, int column) {
     //allocate
     arr = new int*[row];
-    for(int i = 0; i < row; i++)
+    for(int i = 0; i < row+1; i++)
         arr[i] = new int[column];
 
     //initialize zeros
-    for (int i = 0; i < row; i++)
-        for (int j = 0; j < column; j++) 
+    for (int i = 0; i < row+1; i++)
+        for (int j = 0; j < column+1; j++) 
             arr[i][j] = 0;
         
     int h = row / 2;
@@ -215,6 +271,9 @@ int fb_levenshtein (string str1, string str2, int row, int column) {
     //merge results to find the distance
     int ret = merge(h, row, column);
 
+    //bottomUp(str1, str2, row, column);
+
+    /*
     //TEMP
     auto start = high_resolution_clock::now();
     topHalf(ref(a));
@@ -228,15 +287,18 @@ int fb_levenshtein (string str1, string str2, int row, int column) {
     duration = duration_cast<microseconds>(finish - start);
     cout << "Bottom half duration: " << duration.count() << "\n\n\n";
     //TEMP
+    */
 
-    cout << "\n";
-    for (int i = 0; i < row; i++){
-        for (int j = 0; j < column; j++) {
+    /*cout << "\n";
+    for (int i = 0; i < row+1; i++){
+        for (int j = 0; j < column+1; j++) {
             cout << arr[i][j] << " ";
         }
         cout << "\n";
-    }
+    }*/
 
     return ret;
 
 }
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -41,30 +41,31 @@ long long avgExecutionTime (int function, int n, string str1, string str2) {
 }
 
 int main (int argc, char* argv[]) {
-    string str1 = "abcbcb";
-    string str2 = "abccabb";
+    //string str1 = "abcbcb";
+    //string str2 = "abccabb";
 
-    //str1 = randomStringGenerator(30000);
-    //str2 = randomStringGenerator(30000);
+    string str1 = "replace";
+    string str2 = "yyyyyyyy";
 
-    
-    //TODO: currently needs len+1 for LCS, but just len for Levenshtein
+    //str1 = randomStringGenerator(10000);
+    //str2 = randomStringGenerator(10000);
+
     int row = str1.length()+1;
     int column = str2.length()+1;
    
     auto start = high_resolution_clock::now();
-    cout << "Sequential LCS solution: " << sequentialLevenshtein(str1, str2, row, column) << "\n";
+    cout << "Sequential Levenshtein solution: " << sequentialLevenshtein(str1, str2, row, column) << "\n";
     auto finish = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(finish - start);
 
-    cout << "Sequential LCS duration: " << duration.count() << "\n";
+    cout << "Sequential Levenshtein duration: " << duration.count() << "\n";
 
     start = high_resolution_clock::now();
-    cout << "Forward-backward LCS solution: " << fb_levenshtein(str1, str2, row, column) << "\n";
+    cout << "Forward-backward Levenshtein solution: " << fb_levenshtein(str1, str2, row, column) << "\n";
     finish = high_resolution_clock::now();
     duration = duration_cast<microseconds>(finish - start);
 
-    cout << "Forward-backward LCS duration: " << duration.count() << "\n";
+    cout << "Forward-backward Levenshtein duration: " << duration.count() << "\n";
 
     return 0;
 }
