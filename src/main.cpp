41e101a9ca2733e2a362aa719fdca62fe9185315
Luka Bajić <bajicluka01@gmail.com>
2024-07-15 17:45:23 +0200

    Made final measurements for all functions except diagonal

--- src/main.cpp
+++ src/main.cpp
@@ -23,22 +23,6 @@ long long avgExecutionTime (int function, int n, string str1, string str2) {
     int row = str1.length()+1;
     int column = str2.length()+1;
 
-    /*arr = new int*[row];
-    for(int i = 0; i < row+1; i++)
-        arr[i] = new int[column];
-
-    //initialize zeros
-    for (int i = 0; i < row+1; i++)
-        for (int j = 0; j < column+1; j++) 
-            arr[i][j] = 0;
-
-    int h = row / 2;
-    struct args a; 
-    a.s1 = str1;
-    a.s2 = str2;
-    a.row = h;
-    a.col = column;*/
-
     for(int i = 0; i < n; i++) {
         auto start = high_resolution_clock::now();
 
@@ -57,8 +41,6 @@ long long avgExecutionTime (int function, int n, string str1, string str2) {
             case 12: diagonal_levenshtein(str1, str2, row, column); break;
             case 13: fb_levenshtein(str1, str2, row, column); break;
             case 14: fb_levenshtein_space_optimization(str1, str2, row, column); break;
-            //case 15: topHalf_LCS(ref(a)); break;
-            //case 16: bottomHalf_LCS(ref(a)); break;
             default: return 0;
         }
 
@@ -190,21 +172,23 @@ int main (int argc, char* argv[]) {
     string str1 = "replace";
     string str2 = "replace";
 
-    str1 = randomStringGenerator(11, 42);
-    str2 = randomStringGenerator(3, 101);
+    int strLen = 10000;
+
+    str1 = randomStringGenerator(strLen, 42);
+    str2 = randomStringGenerator(strLen, 101);
 
     int row = str1.length()+1;
     int column = str2.length()+1;
    
-    /*auto start = high_resolution_clock::now();
-    cout << "Sequential Levenshtein solution: " << backward_levenshtein(str1, str2, row, column) << "\n";
+    auto start = high_resolution_clock::now();
+    cout << "Sequential Levenshtein solution: " << forward_levenshtein_space_optimization(str1, str2, row, column) << "\n";
     auto finish = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(finish - start);
 
     cout << "Sequential Levenshtein duration: " << duration.count() << "\n";
 
-    start = high_resolution_clock::now();
-    cout << "Forward-backward Levenshtein solution: " << backward_levenshtein_space_optimization(str1, str2, row, column) << "\n";
+    /*start = high_resolution_clock::now();
+    cout << "Forward-backward Levenshtein solution: " << backward_LCS_space_optimization(str1, str2, row, column) << "\n";
     finish = high_resolution_clock::now();
     duration = duration_cast<microseconds>(finish - start);
 
@@ -214,14 +198,20 @@ int main (int argc, char* argv[]) {
 
     //time testing
     //cout << "Forward LCS: "<< avgExecutionTime(1, n_iter, str1, str2) <<"\n";
-    //cout << "Backward LCS: "<< avgExecutionTime(2, n_iter, str1, str2) <<"\n";
-    //cout << "Diagonal LCS: "<< avgExecutionTime(3, n_iter, str1, str2) <<"\n";
-    //cout << "Forward-backward LCS: "<< avgExecutionTime(4, n_iter, str1, str2) <<"\n";
+    //cout << "Forward LCS space optimization: "<< avgExecutionTime(2, n_iter, str1, str2) <<"\n";
+    //cout << "Backward LCS: "<< avgExecutionTime(3, n_iter, str1, str2) <<"\n";
+    //cout << "Backward LCS space optimization: "<< avgExecutionTime(4, n_iter, str1, str2) <<"\n";
+    //cout << "Diagonal LCS: "<< avgExecutionTime(5, n_iter, str1, str2) <<"\n";
+    //cout << "Forward-backward LCS: "<< avgExecutionTime(6, n_iter, str1, str2) <<"\n";
+    //cout << "Forward-backward LCS space optimization: "<< avgExecutionTime(7, n_iter, str1, str2) <<"\n";
     
-    //cout << "Backward Levenshtein: "<< avgExecutionTime(6, n_iter, str1, str2) <<"\n";
-    //cout << "Diagonal Levenshtein: "<< avgExecutionTime(7, n_iter, str1, str2) <<"\n";
-    //cout << "Forward-backward Levenshtein: "<< avgExecutionTime(8, n_iter, str1, str2) <<"\n";
-    //cout << "Forward Levenshtein: "<< avgExecutionTime(5, n_iter, str1, str2) <<"\n";
+    //cout << "Forward Levenshtein: "<< avgExecutionTime(8, n_iter, str1, str2) <<"\n";
+    //cout << "Forward Levenshtein space optimization: "<< avgExecutionTime(9, n_iter, str1, str2) <<"\n";
+    //cout << "Backward Levenshtein: "<< avgExecutionTime(10, n_iter, str1, str2) <<"\n";
+    //cout << "Backward Levenshtein space optimization: "<< avgExecutionTime(11, n_iter, str1, str2) <<"\n";
+    //cout << "Diagonal Levenshtein: "<< avgExecutionTime(12, n_iter, str1, str2) <<"\n";
+    //cout << "Forward-backward Levenshtein: "<< avgExecutionTime(13, n_iter, str1, str2) <<"\n";
+    //cout << "Forward-backward Levenshtein space optimization: "<< avgExecutionTime(14, n_iter, str1, str2) <<"\n";
 
 
     
@@ -244,7 +234,7 @@ int main (int argc, char* argv[]) {
 
 
     
-    testLCSforValues(100, 25);
+    //testLCSforValues(100, 25);
     //cout<<forward_LCS(str1, str2, row, column)<<"\n";
     //cout<<str1<<"\n"<<str2<<"\n";
 
--- src/plots.py
+++ src/plots.py
@@ -1,51 +1,55 @@
 import matplotlib.pyplot as plt
 
 x = [10000, 20000, 30000, 40000, 50000, 60000]
+x2 = [100000, 150000, 200000, 250000]
 
-forwardLevenshtein = [0.549787, 1.901921, 3.791124, 5.911732, 9.231984, 14.285577]
-backwardLevenshtein = [0.255227, 1.174503, 2.675816, 4.022211, 6.941683, 9.575530]
-fbLevenshtein = [0.549787, 1.901921, 3.791124, 5.911732, 9.231984, 14.285577]
-forwardLevenshteinSpace = [0.549787, 1.901921, 3.791124, 5.911732, 9.231984, 14.285577]
-backwardLevenshteinSpace = [0.255227, 1.174503, 2.675816, 4.022211, 6.941683, 9.575530]
-fbLevenshteinSpace = [0.549787, 1.901921, 3.791124, 5.911732, 9.231984, 14.285577]
+forwardLevenshtein =  [0.345842, 1.519301, 3.626702, 6.293141, 9.199267, 13.429229]
+backwardLevenshtein = [0.338444, 1.529102, 3.404311, 6.001849, 9.053014, 13.321899]
+fbLevenshtein =       [0.258670, 1.187424, 2.500014, 4.317889, 6.505424, 9.554413]
 
-forwardLCS = [0.549787, 1.901921, 3.791124, 5.911732, 9.231984, 14.285577]
-backwardLCS = [0.255227, 1.174503, 2.675816, 4.022211, 6.941683, 9.575530]
-fbLCS = [0.255227, 1.174503, 2.675816, 4.022211, 6.941683, 9.575530]
-forwardLCSspace = [0.549787, 1.901921, 3.791124, 5.911732, 9.231984, 14.285577]
-backwardLCSspace = [0.255227, 1.174503, 2.675816, 4.022211, 6.941683, 9.575530]
-fbLCSspace = [0.255227, 1.174503, 2.675816, 4.022211, 6.941683, 9.575530]
+forwardLevenshteinSpace =  [26.395551, 58.962033, 105.193894, 167.749272]
+backwardLevenshteinSpace = [24.271178, 54.160883, 95.668674, 149.694305]
+fbLevenshteinSpace =       [13.435573, 30.358760, 53.150599, 83.564126]
 
-diagonalLevenshtein = [0.549787, 1.901921, 3.791124, 5.911732, 9.231984, 14.285577]
-diagonalLCS = [0.255227, 1.174503, 2.675816, 4.022211, 6.941683, 9.575530]
+forwardLCS =  [0.319667, 1.429334, 3.211196, 6.018281, 10.109169, 12.394485]
+backwardLCS = [0.318628, 1.438086, 3.223706, 6.271317, 9.850569, 12.581315]
+fbLCS =       [0.247322, 1.136386, 2.406986, 4.189387, 7.354680, 9.246377]
+
+forwardLCSspace =  [11.047188, 23.489699, 41.342735, 65.269299]
+backwardLCSspace = [10.564286, 23.267623, 42.785099, 68.833067]
+fbLCSspace =       [6.365848, 14.281909, 26.146862, 40.857753]
+
+#TODO
+diagonalLevenshtein = []
+diagonalLCS =         []
 
 plt.figure(1)
-plt.plot(x, list(zip(forwardLevenshtein, backwardLevenshtein)), label=['forward', 'backward'])
-plt.title('Forward vs Backward Levenshtein comparison')
+plt.plot(x, list(zip(forwardLevenshtein, backwardLevenshtein, fbLevenshtein)), label=['forward', 'backward', 'forward-backward'])
+plt.title('Levenshtein comparison')
 plt.xlabel('string length')
 plt.ylabel('average execution time (seconds)')
 plt.legend()
 
 
 plt.figure(2)
-plt.plot(x, list(zip(forwardLCS, forwardLCSspace)), label=['forward', 'forward with space optimization'])
-plt.title('Forward LCS vs Forward LCS with space optimization')
+plt.plot(x, list(zip(forwardLCS, backwardLCS, fbLCS)), label=['forward', 'backward', 'forward-backward'])
+plt.title('LCS comparison')
 plt.xlabel('string length')
 plt.ylabel('average execution time (seconds)')
 plt.legend()
 
 
 plt.figure(3)
-plt.plot(x, list(zip(forwardLevenshtein, fbLevenshtein)), label=['forward', 'forward-backward'])
-plt.title('Forward vs Forward-backward Levenshtein comparison')
+plt.plot(x2, list(zip(forwardLevenshteinSpace, backwardLevenshteinSpace, fbLevenshteinSpace)), label=['forward', 'backward', 'forward-backward'])
+plt.title('Levenshtein comparison with space optimization')
 plt.xlabel('string length')
 plt.ylabel('average execution time (seconds)')
 plt.legend()
 
 
 plt.figure(4)
-plt.plot(x, list(zip(forwardLCS, diagonalLCS)), label=['forward', 'diagonal'])
-plt.title('Forward vs Diagonal LCS')
+plt.plot(x2, list(zip(forwardLCSspace, backwardLCSspace, fbLCSspace)), label=['forward', 'backward', 'forward-backward'])
+plt.title('LCS comparison with space optimization')
 plt.xlabel('string length')
 plt.ylabel('average execution time (seconds)')
 plt.legend()
